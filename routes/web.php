52cf084684d1e0b3461cd88c80963ff0c79c1d16
Mustafa Usmaniy <104256095+MustafaUsmaniy@users.noreply.github.com>
2024-05-12 08:12:34 +0500

    Install Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
 use App\Http\Controllers\PageController;
-use App\Http\Middleware\Language;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'main'])->name('main');
 Route::get('/detail', [PageController::class, 'detail'])->name('detail');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
-Route::get('/lang/{lang}', function ($lang) {
-    session(['lang' => $lang]);
+Route::get('/lang/{lang}', function($lang){
+    session(['lang'=>$lang]);
 
     return back();
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
